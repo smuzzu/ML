88724e21aabc13458c0ecda91c7d12176b7fe579
sebamuzzu@gmail.com <sebastian@bairesqa.com>
2020-07-06 16:53:27 -0300

    Mejoras saleschecker 8

---
+++ src/com/ml/utils/Product.java
@@ -0,0 +1,12 @@
+package com.ml.utils;
+
+public class Product {
+
+    public String  id;
+    public int     user;
+    public String  title;
+    public int     multiplier;
+    public String  customMessage;
+    public boolean disabled;
+
+}
