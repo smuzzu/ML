2499078b70fb3a75945da4fdef7a7a31cf01db78
sebamuzzu@gmail.com <sebastian@bairesqa.com>
2020-09-14 01:20:27 -0300

    este va con el anterior

---
+++ src/com/ml/utils/Item.java
@@ -0,0 +1,47 @@
+package com.ml.utils;
+
+public class Item {
+
+        public String id;
+        public String title;
+        public String permalink;
+        public double price;
+        public int discount;
+        public boolean advertised;
+        public boolean officialStore;
+        public int shipping;
+        public boolean premium;
+        public int ranking;
+        public int page;
+        public int totalSold;
+        public String sellerName;
+        public int sellerId;
+
+        public Item () {
+            this.advertised=false;
+            this.ranking=10000;
+            this.totalSold=-1;
+            this.price=-1.0;
+            this.discount=-1;
+            this.sellerId=-1;
+        }
+
+        public boolean equals(Object obj) {
+            if (this.id == null || this.id.isEmpty()) {
+                return false;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (!(obj instanceof Item)) {
+                return false;
+            }
+            Item otherItem = (Item) obj;
+            if (otherItem.id == null || otherItem.id.isEmpty()) {
+                return false;
+            }
+            return this.id.equals(otherItem.id);
+        }
+
+
+    }
