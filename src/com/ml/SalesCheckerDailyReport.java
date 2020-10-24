337c142d9d3c652514c5d633e34cbc4fe415a77a
sebamuzzu@gmail.com <sebastian@bairesqa.com>
2020-10-24 12:24:12 -0300

    Sales Daily report version 2

--- src/com/ml/SalesCheckerDailyReport.java
+++ src/com/ml/SalesCheckerDailyReport.java
@@ -1,14 +1,14 @@
 package com.ml;
 
-import com.ml.utils.*;
+import com.ml.utils.GoogleMailSenderUtil;
+import com.ml.utils.HttpUtils;
+import com.ml.utils.Logger;
+import com.ml.utils.MessagesAndSalesHelper;
+import com.ml.utils.Order;
 import org.apache.http.impl.client.CloseableHttpClient;
 
-import java.sql.Date;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalTime;
+
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
 
 public class SalesCheckerDailyReport {
@@ -42,9 +42,6 @@ public class SalesCheckerDailyReport {
 
         Collections.sort(completePendingOrders);
 
-        //esto es el informe diario
-
-// step 1
         String html = "<h1>Correo</h1><table border=1>";
         for (Order pendingOrder: completePendingOrders) {  //viene de la base info limitada
             if (pendingOrder.shippingType==Order.CORREO && pendingOrder.readyForSending){
@@ -103,12 +100,35 @@ public class SalesCheckerDailyReport {
                 html+="<td width=200px>&nbsp;</td></tr>";
             }
         }
+        html += "</table><br/><br/><br/>";
+
+        html += "<h1>Acordar / Retira</h1><table border=1>";
+        for (Order pendingOrder: completePendingOrders) {  //viene de la base info limitada
+            if (pendingOrder.shippingType==Order.ACORDAR){
+                html+="<tr><td width=500px>"+pendingOrder.productTitle+"<br/>";
+                if (pendingOrder.productVariationText!=null && !pendingOrder.productVariationText.isEmpty()
+                        && !pendingOrder.productVariationText.equals("N/A")) {
+                    html += pendingOrder.productVariationText+"<br/>";
+                }
+                if (pendingOrder.productKeyAttributes!=null && !pendingOrder.productKeyAttributes.isEmpty()
+                        && !pendingOrder.productKeyAttributes.equals("N/A")) {
+                    html+=pendingOrder.productKeyAttributes+"<br/>";
+                }
+                html += "Cantidad: " + pendingOrder.productQuantity + "<br/>";
+                html += "Comprador: " + pendingOrder.buyerFirstName + " " + pendingOrder.buyerLastName+"<br/>";
+                if (pendingOrder.buyerPhone!=null && !pendingOrder.buyerPhone.isEmpty()){
+                    html += "Telefono: " + pendingOrder.buyerPhone;
+                }
+                html+="<td width=200px>&nbsp;</td></tr>";
+            }
+        }
+
 
         html += "</table>";
 
-
-        boolean b=false;
-
+        String mailTitle="Reporte de entregas pendientes";
+        String destinationAdress="sebamuzzu2@gmail.com,centroequipamientos@centroequipamientos.com.ar";
+        GoogleMailSenderUtil.sendMail(mailTitle, html, destinationAdress,null);
 
     }
 
