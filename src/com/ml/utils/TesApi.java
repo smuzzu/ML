7148b43d68e14e69610e497e4ba4398967b7b57c
sebamuzzu@gmail.com <sebastian@bairesqa.com>
2021-05-06 21:02:44 -0300

    Mejoras en TestApi

--- src/com/ml/utils/TesApi.java
+++ src/com/ml/utils/TesApi.java
@@ -14,6 +14,8 @@ public class TesApi {
     private static String QUEFRESQUETE=SData.getQuefresquete();
     private static String sampleCustomerUrl="https://api.mercadolibre.com/sites/MLA/search?nickname=modular+drawers";
     private static String baseProductUrl="https://api.mercadolibre.com/items/";
+    private static String baseOrderUrl="https://api.mercadolibre.com/orders/";
+    private static String baseShippingUrl="https://api.mercadolibre.com/shipments/";
 
 
     private static String getCategory(String productId,String apiUser){
@@ -28,7 +30,29 @@ public class TesApi {
     }
 
 
+    private static JSONObject getShipping(String orderId,String apiUser){
+        JSONObject orderObject = getOrder(orderId,QUEFRESQUETE);
+        JSONObject shippingObject = orderObject.getJSONObject("shipping");
+        String shippingId=""+shippingObject.getLong("id");
+        String shippingtUrl=baseShippingUrl+shippingId;
+        shippingObject = HttpUtils.getJsonObjectUsingToken(shippingtUrl,HttpUtils.buildHttpClient(),apiUser,false);
+        return shippingObject;
+    }
+
+    private static JSONObject getOrder(String orderId,String apiUser){
+        String result="N/A";
+        String productUrl=baseOrderUrl+orderId;
+        JSONObject  orderObject = HttpUtils.getJsonObjectUsingToken(productUrl,HttpUtils.buildHttpClient(),apiUser,false);
+        return orderObject;
+    }
+
+
+
     public static void main(String[] args){
+
+        String oderId="4485479390";
+        //JSONObject orderObject = getOrder(oderId,QUEFRESQUETE);
+        JSONObject shippingObject = getShipping(oderId,QUEFRESQUETE);
 
         String productId="MLA901398938";
         getCategory(productId,SOMOS);
